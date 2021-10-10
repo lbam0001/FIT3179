9dc35b90d37d1ec28c220d503579bd0ea1886853
lbam0001 <lbam0001@student.monash.edu>
2021-10-10 18:42:49 +1100

    Add total immigration chloropleth map of Australia

--- vis2.js
+++ vis2.js
@@ -1,4 +1,4 @@
-var vg_1 = "pop_map.vg.json";
+var vg_1 = "migration_map.vg.json";
 vegaEmbed("#pop_map", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
