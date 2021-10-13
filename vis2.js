0b59dd236393fa1076a9e6490c6297e30c4f3900
lbam0001 <lbam0001@student.monash.edu>
2021-10-14 09:59:36 +1100

    Update web page with new charts

--- vis2.js
+++ vis2.js
@@ -1,4 +1,14 @@
-var vg_1 = "migration_map.vg.json";
-vegaEmbed("#pop_map", vg_1).then(function(result) {
+var vg_1 = "urban_map.vg.json";
+vegaEmbed("#urban_pop_map", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "migration_map.vg.json";
+vegaEmbed("#imm_pop_map", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "pop_pyramid.vg.json";
+vegaEmbed("#pop_pyramid", vg_3).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
