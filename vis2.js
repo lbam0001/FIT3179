326540104df1dfdab9322a1c4682c32ef1065d62
lbam0001 <lbam0001@student.monash.edu>
2021-10-18 15:22:34 +1100

    Remove unnecessary json files and rename key files
    Improve human readability by renaming and removing files.

--- vis2.js
+++ vis2.js
@@ -3,17 +3,13 @@ vegaEmbed("#urban_pop_map", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_2 = "migration_map.vg.json";
-vegaEmbed("#imm_pop_map", vg_2).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
 
-var vg_4 = "origin_year.vg.json";
+var vg_4 = "migration_charts.vg.json";
 vegaEmbed("#imm_pop_map", vg_4).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_5 = "pop_charts.vg.json";
+var vg_5 = "birth_charts.vg.json";
 vegaEmbed("#birth_charts", vg_5).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
